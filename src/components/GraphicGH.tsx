dde008416f47031dcd75cf51bc61ba958c529095
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2025-09-07 12:00:40 -0500

    Data-AOS agregado el componente de Contribuciones

--- src/components/GraphicGH.tsx
+++ src/components/GraphicGH.tsx
@@ -2,7 +2,7 @@ import GitHubCalendar from "react-github-calendar"
 
 export default function GraphicGH() {
   return (
-    <div className="flex justify-center m-2 lg:m-0 p-5 rounded-xl bg-[#0d1117] text-white">
+    <div className="flex justify-center m-2 lg:m-0 p-5 rounded-xl bg-[#0d1117] text-white" data-aos="fade-up">
       <GitHubCalendar
         username="MiguelRequenaR"
       />
